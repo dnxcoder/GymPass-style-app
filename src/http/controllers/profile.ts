db58392e89b3a6acc312b9e4dbe92017d836f866
Denis Coder <dnxcoder@gmail.com>
2023-07-30 02:32:39 -0500

    profile controller

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,10 +1,12 @@
+import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use-case";
 import { FastifyRequest, FastifyReply } from "fastify";
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  //verifies if token is valid
-  await request.jwtVerify();
+  const userId = request.user.sub;
 
-  console.log(request.user.sub);
+  const gerUserProfile = makeGetUserProfileUseCase();
 
-  return reply.status(200).send();
+  const user = await gerUserProfile.execute(userId);
+
+  return reply.status(200).send({ ...user, password_hash: undefined });
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 import { registerController } from "./controllers/register";
 import { authenticateController } from "./controllers/authenticate";
 import { profile } from "./controllers/profile";
+import { verifyJWT } from "./middlewares/verify-jwt";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", registerController);
@@ -10,5 +11,5 @@ export async function appRoutes(app: FastifyInstance) {
 
   /* Authenticated Routes */
 
-  app.get("/me", profile)
+  app.get("/me", { onRequest: [verifyJWT] }, profile);
 }
