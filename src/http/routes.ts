c573ad0f3b590932c92a2d6b181827625a6610fe
Denis Coder <dnxcoder@gmail.com>
2023-07-30 01:57:00 -0500

    Principles of authentication

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,14 @@
 import { FastifyInstance } from "fastify";
 import { registerController } from "./controllers/register";
 import { authenticateController } from "./controllers/authenticate";
+import { profile } from "./controllers/profile";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", registerController);
 
   app.post("/sessions", authenticateController);
+
+  /* Authenticated Routes */
+
+  app.get("/me", profile)
 }
